e2efdc124ab94fbc3a972b3ed18070bb1cbcf3ce
bigjihua <bigjihua@gmail.com>
2024-04-19 11:39:06 +0800

    2024年4月19日 修复已知BUG

--- README.md
+++ README.md
@@ -1,27 +1,41 @@
 # jihau_top_vue
 
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
 
 # 项目已部署，点击下方访问jihau.top
-See (https://jihau.top).
+
+See (<https://jihau.top>).
+
+## 代码直接跑是跑不起来的
+
+请查阅.gitignore 里所忽略的文件
+/src/Views/Page/10010.vue --- 私人定制页面
+/src/assets/font --- 字体文件
+/src/assets/css/typeface.css --- 字体配置文件
+/public/ckeditor --- 富文本编译器 需要自行下载适配Vue2.*版本并正确的解压放置public文件夹下
---
+++ src/layout/FooterBar/Footer.vue
@@ -0,0 +1,77 @@
+<template>
+  <div id="" class="footer">
+    <div id="icp">
+      <router-link to="/">
+        <img src="https://jihau.top/img/logo.png" alt="站点logo" width="30" height="30" />
+      </router-link>
+      网页所有人: <span class="coker"></span>&nbsp;邮编: <br />
+      ICP备案号:<a href="https://beian.miit.gov.cn"></a><br />
+      <p class="icp_alt">
+        建议您使用Chrome、Firefox、Edge、IE10及以上版本等浏览器并把缩放倍率调整为110-125%浏览本网站
+        |
+        <router-link to="/page/YSZC">隐私政策</router-link>
+      </p>
+      <a title="返回顶部" @click="totop"><img src="https://jihau.top/img/zujian/upupday.png" alt="返回顶部" width="40px"
+          height="40px" class="nonetop" ref="totop" /></a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted () {
+    window.addEventListener('scroll', this.handleScroll)
+  },
+  // 方法
+  methods: {
+    totop () {
+      document.documentElement.scrollTop = 0
+      document.body.scrollTop = 0
+    },
+    handleScroll () {
+      const indexHeader = this.$refs.totop
+      if (indexHeader) {
+        const scrollTop = document.documentElement.scrollTop || window.pageYOffset || document.body.scrollTop
+        if (scrollTop >= 650) {
+          indexHeader.setAttribute('class', 'top')
+        }
+        if (scrollTop === 0) {
+          indexHeader.setAttribute('class', 'nonetop')
+        }
+      }
+    }
+  },
+  name: 'FooTer'
+}
+</script>
+
+<style lang="less">
+.footer {
+  width: 100%;
+  text-align: center;
+  color: #99a2b4;
+  padding: 5px;
+  border-radius: 5px;
+  background-color: #fff;
+  margin: 10px 0 15px 0;
+  box-shadow: rgb(0 0 0 / 0%) 0px 0px 0px 0px, rgb(0 0 0 / 0%) 0px 0px 0px 0px, rgb(0 0 0 / 10%) 0px 10px 15px -3px, rgb(0 0 0 / 5%) 0px 4px 6px -2px;
+  border-radius: 5px;
+  border-radius: 5px;
+  padding: 10px;
+
+  .top {
+    right: 15px;
+    position: fixed;
+    bottom: 100px;
+    z-index: 99;
+    width: 50px;
+    height: auto;
+    text-align: center;
+    cursor: pointer;
+  }
+
+  .nonetop {
+    display: none;
+  }
+}
+</style>
